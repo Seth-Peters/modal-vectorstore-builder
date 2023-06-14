dfbf98a4070478e694f56925684915a283eac89b
Seth Peters <seth.peters11@gmail.com>
2023-06-14 08:24:38 +0300

    Added embeddings in parallel. Updated README. Working pipeline now

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # modal-vectorstore-builder
+
 Basic ingest, embed, and vectorstore pipeline in built with Modal to autoscale to the amount of data.
+
+Stores a vectorstore of the text locally in `vectorstore/`.
+
+## Commands to run
+
+`modal run ingest.py`
--- ingest.py
+++ ingest.py
@@ -1,13 +1,24 @@
-import modal
-from modal import Image
+from modal import Image, Stub
+
+
+def download_model():
+    from sentence_transformers import SentenceTransformer
+
+    SentenceTransformer(model_name_or_path="all-mpnet-base-v2", cache_folder="/models")
+
+
+transformer_image = (
+    Image.debian_slim().pip_install("sentence_transformers", "langchain").run_function(download_model)
+)
 
 image = Image.debian_slim().pip_install(
     "langchain",
     "pypdf",
     "faiss-cpu",
+    "sentence_transformers",
 )
 
-stub = modal.Stub(
+stub = Stub(
     name="etl-pdfs",
     image=image,
 )
@@ -25,15 +36,32 @@ def main(json_path="data/llm-papers.json"):
 
     pdf_urls = [pdf["url"] for pdf in pdf_infos]
 
+    # pdf_urls = pdf_urls[:50]
+
     documents = []
     for result in load_pdf_url.map(pdf_urls, return_exceptions=True):
         documents.append(result)
 
+    # print("documents: ", len(documents))
+    # print("documents[0]: ", documents[0])
+
     documents = sum(documents, [])
 
-    split_documents = split_docs(documents)
+    split_documents = split_docs.call(documents)
 
-    create_vectorstore_of_text(split_documents)
+    embeddings_list = []
+    for results in embed_single_document.map(split_documents, return_exceptions=True):
+        embeddings_list.append(results)
+
+    upsert_to_vectorstore = []
+    for text, embedding in zip(split_documents, embeddings_list):
+        upsert_to_vectorstore.append((text.page_content, embedding))
+
+    # print("split_documents: ", len(split_documents))
+    # print("embeddings_list: ", len(embeddings_list))
+    # print("upsert_to_vectorstore: ", len(upsert_to_vectorstore))
+
+    create_vectorstore_of_text(upsert_to_vectorstore)
 
     # return vectorstore
 
@@ -58,11 +86,22 @@ def split_docs(docs):
     return docs
 
 
-@stub.function(image=image)
+@stub.function(image=transformer_image)
+def embed_single_document(doc: str) -> list:
+    from sentence_transformers import SentenceTransformer
+
+    model = SentenceTransformer(model_name_or_path="all-mpnet-base-v2", cache_folder="/models")
+    text = doc.page_content
+    embeddings = model.encode(text)
+
+    return embeddings
+
+
+@stub.function(image=image, retries=3)
 def create_vectorstore_of_text(docs):
-    from langchain.embeddings import FakeEmbeddings
     from langchain.vectorstores.faiss import FAISS
+    from langchain.embeddings import HuggingFaceEmbeddings
 
-    faiss_index = FAISS.from_documents(docs, embedding=FakeEmbeddings(size=200))
+    faiss_index = FAISS.from_embeddings(docs, embedding=HuggingFaceEmbeddings())
 
-    faiss_index.save_local("vectorstore/llm-papers-faiss")
+    faiss_index.save_local("vectorstore/llm-papers-faiss-testing")
